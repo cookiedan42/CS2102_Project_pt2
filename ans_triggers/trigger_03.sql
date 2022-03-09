ece56bf92f7ffd15fac952cd2f11d7bbbd48dfe6
cookiedan42 <cookiedan42@gmail.com>
2022-03-09 23:08:30 +0800

    trigger 3

--- ans_triggers/trigger_03.sql
+++ ans_triggers/trigger_03.sql
@@ -1,16 +1,25 @@
 /*
-Coupon related:
-(3) A coupon can only be used on an order whose total amount (before the coupon is applied) exceeds
-the minimum order amount.
+does EXCEED allow for equal?
 */
-CREATE OR REPLACE FUNCTION coumpon_minimum_func() RETURNS TRIGGER
+
+
+/*
+Coupon related:
+(3) A coupon can only be used on an order whose total amount (before the coupon is applied) 
+exceeds the minimum order amount.
+*/
+
+CREATE OR REPLACE FUNCTION coupon_minimum_func() RETURNS TRIGGER
 AS $$
 BEGIN 
-    RETURN NULL;
+    IF NEW.payment_amount > ALL( SELECT  min_order_amount FROM coupon_batch WHERE id = NEW.coupon_id)
+        THEN return NEW;
+    ELSE
+        raise exception 'order % does not have any orderlines',NEW;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER coumpon_minimum_trigger
-BEFORE DELETE ON shop /*syntax placeholder*/
-/* <BEFORE AFTER INSTEAD OF> <INSERT/DELETE/UPDATE> ON <TABLE> */
-FOR EACH ROW EXECUTE FUNCTION coumpon_minimum_func();
+CREATE OR REPLACE TRIGGER coupon_minimum_trigger
+AFTER INSERt ON orders
+FOR EACH ROW EXECUTE FUNCTION coupon_minimum_func();
---
+++ tests/test_trigger_03.sql
@@ -0,0 +1,33 @@
+INSERT into users VALUES
+    (1, 'user_address_1', 'user_name_1', FALSE);
+INSERT into category values 
+    (1, 'category_1',NULL);
+INSERT into manufacturer values 
+    (1, 'manufacturer_1','country_1');
+INSERT into product values 
+    (1, 'name', 'desc', 1, 1);
+INSERT into shop values 
+    (1, '1');
+INSERT into sells values 
+    (1, 1, '2022-01-01 01:01:01', 1, 1);
+INSERT into coupon_batch values
+    (1, '2022-01-01', '2022-01-01', 1, 1);
+INSERT into issued_coupon values
+    (1, 1);
+
+SELECT * FROM coupon_batch;
+
+-- cannot insert
+INSERT INTO orders values
+    (1,1,1,'address',0);
+SELECT * from orders;
+
+-- cannot insert
+INSERT INTO orders values
+    (1,1,1,'address',1);
+SELECT * from orders;
+
+-- can insert
+INSERT INTO orders values
+    (1,1,1,'address',1.1);
+SELECT * from orders;
