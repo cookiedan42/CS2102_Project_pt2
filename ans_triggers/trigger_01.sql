56afed1b9ce148e97aac883d70204af2b8c18984
cookiedan42 <cookiedan42@gmail.com>
2022-03-09 22:49:20 +0800

    trigger_2

--- ans_triggers/trigger_01.sql
+++ ans_triggers/trigger_01.sql
@@ -12,15 +12,11 @@ EXECUTE FUNCTION shop_minimum_func();
 
 CREATE OR REPLACE FUNCTION shop_minimum_func() RETURNS TRIGGER 
 AS $$
-DECLARE 
-    product_count   INTEGER;
 BEGIN 
-
     IF (EXISTS( SELECT 1 FROM sells WHERE NEW.id = sells.shop_id))
         THEN RETURN NEW;
     ELSE raise exception 'shop % does not sell anything',NEW;
     END IF;
-
 END;
 $$ LANGUAGE plpgsql;
 
--- ans_triggers/trigger_02.sql
+++ ans_triggers/trigger_02.sql
@@ -3,18 +3,21 @@ Order related:
 (2) An order must involve one or more products from one or more shops.
 order must have some orderline
 */
+
+
+CREATE CONSTRAINT TRIGGER order_minimum_trigger
+AFTER INSERT ON orders
+DEFERRABLE INITIALLY DEFERRED
+FOR EACH ROW 
+EXECUTE FUNCTION order_minimum_func();
+
 CREATE OR REPLACE FUNCTION order_minimum_func() RETURNS TRIGGER
 AS $$
 BEGIN 
-/*
-if order exists and this is the last one, don't delete
-*/
-    RETURN NULL;
+    IF (EXISTS( SELECT 1 FROM orderline WHERE NEW.id = orderline.order_id))
+        THEN RETURN NEW;
+    ELSE raise exception 'order % does not have any orderlines',NEW;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER order_minimum_trigger
-BEFORE UPDATE ON orderline /*syntax placeholder*/
-/* <BEFORE AFTER INSTEAD OF> <INSERT/DELETE/UPDATE> ON <TABLE> */
-FOR EACH ROW 
-EXECUTE FUNCTION order_minimum_func();
