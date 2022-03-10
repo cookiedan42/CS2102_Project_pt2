3e0174592be168756bc87496e66df4f3f7563a08
cookiedan42 <cookiedan42@gmail.com>
2022-03-10 20:49:00 +0800

    trigger 8

--- ans_triggers/trigger_08.sql
+++ ans_triggers/trigger_08.sql
@@ -2,14 +2,27 @@
 Comment related:
 (8) A comment is either a review or a reply, not both (non-overlapping and covering).
 */
+
+CREATE CONSTRAINT TRIGGER comment_type_trigger
+AFTER INSERT ON comment
+DEFERRABLE INITIALLY DEFERRED
+FOR EACH ROW EXECUTE FUNCTION comment_type_func();
+
 CREATE OR REPLACE FUNCTION comment_type_func() RETURNS TRIGGER
 AS $$
+DECLARE
+    review_count    INTEGER;
+    reply_count     INTEGER;
 BEGIN 
-    RETURN NULL;
+    select count(*) into review_count from review where id = NEW.id;
+    select count(*) into reply_count  from reply where id = NEW.id;
+
+    IF (review_count + reply_count) = 1
+        THEN return NEW;
+    ELSE 
+        raise exception '% reviews and replies share the same comment id of %',
+            (review_count + reply_count), NEW.id;
+    END IF;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER comment_type_trigger
-BEFORE DELETE ON shop /*syntax placeholder*/
-/* <BEFORE AFTER INSTEAD OF> <INSERT/DELETE/UPDATE> ON <TABLE> */
-FOR EACH ROW EXECUTE FUNCTION comment_type_func();
---
+++ tests/test_trigger_08.sql
@@ -0,0 +1,86 @@
+INSERT into users VALUES
+    (1, 'user_address_1', 'user_name_1', FALSE);
+INSERT into category values 
+    (1, 'category_1',NULL);
+INSERT into employee values
+    (0, 'automated',0);
+INSERT into manufacturer values 
+    (1, 'manufacturer_1','country_1');
+INSERT into product values 
+    (1, 'name', 'desc', 1, 1);
+INSERT into shop values 
+    (1, '1');
+INSERT into coupon_batch values
+    (1, '2022-01-01', '2022-01-01', 1, 1);
+INSERT into issued_coupon values
+    (1, 1);
+INSERT into orders values 
+    (1,1,1,'shipping address',1);  
+
+
+-- cannot insert neither
+INSERT into sells values 
+    (1, 1, '2022-01-01 01:01:01', 1, 1);
+INSERT into orderline values 
+    (1,1,1,'2022-01-01 01:01:01',1,1,'being_processed',NULL);
+INSERT into comment values
+    (1,1);
+select * from comment;
+select * from review;
+select * from reply;
+
+-- cannot insert both
+INSERT into sells values 
+    (1, 1, '2022-01-01 01:01:02', 1, 1);
+INSERT into orderline values 
+    (1,1,1,'2022-01-01 01:01:02',1,1,'shipped','2022-01-01');
+
+BEGIN TRANSACTION;
+INSERT into comment values
+    (2,1);
+INSERT INTO review values
+    (2, 1, 1, 1, '2022-01-01 01:01:02');
+INSERT into reply values 
+    (2,2);
+
+COMMIT;
+
+select * from comment;
+select * from review;
+select * from reply;
+
+
+
+
+-- can insert review
+INSERT into sells values 
+    (1, 1, '2022-01-01 01:01:03', 1, 1);
+INSERT into orderline values 
+    (1,1,1,'2022-01-01 01:01:03',1,1,'shipped','2022-01-01');
+BEGIN TRANSACTION;
+INSERT into comment values
+    (3,1);
+INSERT INTO review values
+    (3, 1, 1, 1, '2022-01-01 01:01:03');
+COMMIT;
+select * from comment;
+select * from review;
+select * from reply;
+
+
+
+-- can insert reply
+INSERT into sells values 
+    (1, 1, '2022-01-01 01:01:04', 1, 1);
+INSERT into orderline values 
+    (1,1,1,'2022-01-01 01:01:04',1,1,'shipped','2022-01-01');
+BEGIN TRANSACTION;
+INSERT into comment values
+    (4,1);
+INSERT INTO reply values
+    (4,3);
+COMMIT;
+select * from comment;
+select * from review;
+select * from reply;
+
